a7651cb904dd8498d2e19fa34dca8c349812a4c8
never-ljy <1249094460@qq.com>
2020-10-28 13:53:40 +0800

    第一次提交

---
+++ src/main/java/cn/lzl/MallSwarmApplication.java
@@ -0,0 +1,13 @@
+package cn.lzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSwarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSwarmApplication.class, args);
+    }
+
+}
