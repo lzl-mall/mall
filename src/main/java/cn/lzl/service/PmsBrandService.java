4bad47260c6e9e3bb4d9a108057be1586c5f351f
never-ljy <1249094460@qq.com>
2020-11-03 17:35:30 +0800

    修改pom

---
+++ src/main/java/cn/lzl/service/PmsBrandService.java
@@ -0,0 +1,26 @@
+package cn.lzl.service;
+
+
+
+
+import cn.lzl.mbg.model.PmsBrand;
+
+import java.util.List;
+
+/**
+ * PmsBrandService
+ * Created by ljy on 2020.11.3.
+ */
+public interface PmsBrandService {
+    List<PmsBrand> listAllBrand();
+
+    int createBrand(PmsBrand brand);
+
+    int updateBrand(Long id, PmsBrand brand);
+
+    int deleteBrand(Long id);
+
+    List<PmsBrand> listBrand(int pageNum, int pageSize);
+
+    PmsBrand getBrand(Long id);
+}
